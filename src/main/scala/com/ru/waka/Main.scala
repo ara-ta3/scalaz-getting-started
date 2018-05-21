971ae47859a1446c4ead3c504d1a01e95c88f02a
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2018-05-21 19:35:27 +0900

    Hello

--- src/main/scala/com/ru/waka/Main.scala
+++ src/main/scala/com/ru/waka/Main.scala
@@ -1,7 +1,41 @@
 package com.ru.waka
 
+import scalaz._
+import scalaz.Scalaz._
+
+/**
+  * with https://gist.github.com/rirakkumya/2724922
+  */
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+    val res0 = List(
+      "H".right,
+      "e".right,
+      "l".right,
+      "l".right,
+      "o".right
+    ): List[Exception \/ String]
+    println(res0.sequence[({ type X[A] = Exception \/ A })#X, String])
+
+    val res1 = List(
+      "H".right,
+      "e".right,
+      "l".right,
+      "l".right,
+      "o".right,
+      new Exception("World").left
+    )
+    println(res1)
+    println(res1.sequence[({ type X[A] = Exception \/ A })#X, String])
+
+    val res2 = List(
+      "H".some,
+      "e".some,
+      "l".some,
+      "l".some,
+      "o".some
+    )
+    println(res2)
+    println(res2.sequence)
   }
 }
