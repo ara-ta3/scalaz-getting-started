c408d8acd44bd5ead6056246fe8a268447c84a44
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2018-05-21 19:16:27 +0900

    Hello world

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
 SBT=sbt
 
+
+run:
+	$(SBT) run
+
 compile:
 	$(SBT) compile
+
+console:
+	$(SBT) console
---
+++ src/main/scala/com/ru/waka/Main.scala
@@ -0,0 +1,7 @@
+package com.ru.waka
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
